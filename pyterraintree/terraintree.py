1378c071ece799a4534f7ba7fc82450aae748241
zacharyburnett <zachary.r.burnett@gmail.com>
2022-02-20 08:48:38 -0500

    add extension calls to Python object

--- pyterraintree/terraintree.py
+++ pyterraintree/terraintree.py
@@ -3,6 +3,8 @@ from os import PathLike
 import subprocess
 import sys
 
+import Terrain_Trees
+
 
 class TreeType(Enum):
     PR_TREE = 'pr'
@@ -32,6 +34,17 @@ class TerrainTree:
 
     @classmethod
     def read(cls, path: PathLike) -> 'TerrainTree':
+        # TODO specify tree threshold values
+        py_tree = Terrain_Trees.PyPRT_Tree(1, 4)
+        py_mesh = Terrain_Trees.PyMesh()
+        py_tree.get_mesh(py_mesh)
+
+        py_reader = Terrain_Trees.PyReader()
+        py_reader.Py_read_mesh(py_tree, str(path))
+
+        py_tree.build_tree()
+
+        # TODO build Python object from tree object
         return cls()
 
     def write(self, path: PathLike):
